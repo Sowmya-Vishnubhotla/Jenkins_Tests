29200e3c830d0d8dbd05207ee392e69cb9d9ee03
Vishnubhotla <v.sowmya406@gmail.com>
2020-10-28 14:42:49 +0530

    Added a file

---
+++ Square.py
@@ -0,0 +1,6 @@
+def Find_square(b):
+    print("Please input the number",b)
+    Square_of_the_number = b*b
+    return Square_of_the_number
+Square=Find_square(16)
+print("Square of the number is:",Square)
